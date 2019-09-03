2b986bde829bfaf872b327d793e23b831af18fcc
DavidRNogueira <53921681+DavidRNogueira@users.noreply.github.com>
2019-09-02 21:06:52 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+
+for (var i = 0; i < 7; i++){
+  document.querySelectorAll("button")[i].addEventListener("click",function(){
+
+    var letterOnButton = this.innerHTML;
+    keyboardClick(letterOnButton)
+
+})}
+
+document.addEventListener("keydown", function(event){keyboardClick(event.key);});
+
+
+
+function keyboardClick(key){
+    switch (key){
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3")
+        tom1.play()
+        break;
+
+      case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play()
+      break;
+
+      case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play()
+      break;
+
+      case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play()
+      break;
+
+      case "j":
+      var tom2 = new Audio("sounds/snare.mp3")
+      tom2.play()
+      break;
+
+      case "k":
+      var tom2 = new Audio("sounds/crash.mp3")
+      tom2.play()
+      break;
+
+      case "l":
+      var tom2 = new Audio('sounds/kick-bass.mp3')
+      tom2.play()
+      break;
+  }}
